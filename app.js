9dd0db885c3940f7d068db3d225756e8c69fbf00
justint6 <33295519+justint6@users.noreply.github.com>
2024-03-30 22:13:38 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const fs = require('fs');
 
-app.use(express.static('../dist/devfile-stack-nodejs-angular'));
+app.use(express.static('./dist/devfile-stack-nodejs-angular'));
 
 app.get('/', (req, res) => {
     res.send('starting application!');
